fd24ca5dc069cbc0d0cf574c34c765bf822edccd
Ilya030290 <anoshko90@mail.ru>
2022-08-12 16:26:55 +0300

    add Main page

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import QuestionBlock from "../../components/QuestionBlock/QuestionBlock";
+import AnswersAndDescription from "../../components/AnswersAndDescriptionBlock/AnswersAndDescription";
+
+
+const MainPage = () => {
+    return (
+        <>
+            <QuestionBlock/>
+            <AnswersAndDescription/>
+        </>
+    );
+};
+
+export default MainPage;
