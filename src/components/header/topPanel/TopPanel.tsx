420b1bae3649a5a870d0f4c0848956eed40e8689
Ilya030290 <anoshko90@mail.ru>
2022-08-16 17:22:01 +0300

    changes for topPanel

--- src/components/header/topPanel/TopPanel.tsx
+++ src/components/header/topPanel/TopPanel.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import {useAppSelector} from "../../../redux/store";
 import style from './TopPanel.module.css';
 
+
 const TopPanel = () => {
+
+    const score = useAppSelector(state => state.score.score);
+
     return (
         <div className={style.topPanel}>
             <div className={style.logo}></div>
             <h5 className={style.score}>
-                Score: <span>0</span>
+                Score: <span>{score}</span>
             </h5>
         </div>
     );
