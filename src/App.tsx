8a0b90fda09562a624085ad368aef5f8441e5431
Ilya030290 <anoshko90@mail.ru>
2022-08-16 17:22:14 +0300

    changes for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import AppRouter from "./components/appRouter/AppRouter";
+import React, {useEffect} from 'react';
+import {useAppDispatch, useAppSelector} from "./redux/store";
+import {fetchBirdsData} from "./redux/birdsReducer";
+import Header from "./components/header/Header";
+import GamePage from "./pages/gamePage/GamePage";
+import Loader from "./components/common/loader/Loader";
 import style from './App.module.css';
 
 
 const App = () => {
+
+    const dispatch = useAppDispatch();
+    const status = useAppSelector(state => state.app.status);
+
+    useEffect(() => {
+        dispatch(fetchBirdsData());
+    }, [dispatch])
+
+    if(status === 'loading') return <Loader/>
+
     return (
         <div className={style.appContainer}>
-            <AppRouter/>
+            <Header/>
+            <GamePage/>
         </div>
     );
 }
