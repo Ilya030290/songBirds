c336428b51b76dca838911580884dc3f9b83a182
Ilya030290 <anoshko90@mail.ru>
2022-08-19 11:10:36 +0300

    add constants

---
+++ src/common/constants.ts
@@ -0,0 +1,14 @@
+export const statuses = {
+    default: 'default',
+    fail: 'fail',
+    success: 'success',
+};
+
+export const indicators = [
+    { id: 1, status: statuses.default },
+    { id: 2, status: statuses.default },
+    { id: 3, status: statuses.default },
+    { id: 4, status: statuses.default },
+    { id: 5, status: statuses.default },
+    { id: 6, status: statuses.default },
+];
