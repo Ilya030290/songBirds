473ee19dab08fe1853418fa5e9170a37650dfafa
Ilya030290 <anoshko90@mail.ru>
2022-08-19 11:13:10 +0300

    changes for Answers

--- src/components/gameBlock/answers/Answers.tsx
+++ src/components/gameBlock/answers/Answers.tsx
@@ -3,15 +3,16 @@ import {useAppSelector} from "../../../redux/store";
 import style from './Answers.module.css';
 
 
+
 const Answers = () => {
 
-    const birdsOfFirstSection = useAppSelector(state => state.birds[0].birds);
+    const birds = useAppSelector(state => state.game.birdsData[0].birds);
 
     return (
         <div className={style.answers}>
             <ul className={style.answersList}>
                 {
-                    birdsOfFirstSection.map(bird => <li key={bird.id}><span/>{bird.name}</li>)
+                    birds.map(bird => <li key={bird.id}><span/>{bird.name}</li>)
                 }
             </ul>
         </div>
