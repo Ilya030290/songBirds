eb7794d8b861f9b55381714a50f1ac126b7a1bb2
Ilya030290 <anoshko90@mail.ru>
2022-08-19 11:15:12 +0300

    changes for QuestionBlock

--- src/components/questionBlock/QuestionBlock.tsx
+++ src/components/questionBlock/QuestionBlock.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import AudioPlayer from "./audioPlayer/AudioPlayer";
+import image from '../../assets/images/bird.06a46938.jpg';
 import style from './QuestionBlock.module.css';
 
 
 const QuestionBlock = () => {
     return (
         <div className={style.questionContainer}>
-            <img src={'https://birds-quiz.netlify.app/static/media/bird.06a46938.jpg'}  alt={'bird'}/>
+            <img src={image}  alt={'bird'}/>
             <div className={style.listContainer}>
                 <div className={style.list}>
                     <h3>******</h3>
