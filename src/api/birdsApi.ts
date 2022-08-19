a99e8fba5430f7a18f1d8dfe240b9f202643fb54
Ilya030290 <anoshko90@mail.ru>
2022-08-19 11:11:24 +0300

    some change in api

--- src/api/birdsApi.ts
+++ src/api/birdsApi.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-import {BirdsSection} from "../redux/birdsReducer";
+import {BirdsSectionType} from "../types/types";
 
 
 //Api
 export const BirdsApi = {
     fetchBirds: () => {
-        return axios.get<BirdsSection[]>('https://raw.githubusercontent.com/Ilya030290/social_network/master/birds/data.json')
+        return axios.get<BirdsSectionType[]>('https://raw.githubusercontent.com/Ilya030290/social_network/master/birds/data.json')
     }
 }
