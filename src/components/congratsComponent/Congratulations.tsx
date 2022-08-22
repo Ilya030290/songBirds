a39b9d082e849a3578de385bcb692753d73a78a0
Ilya030290 <anoshko90@mail.ru>
2022-08-22 10:26:01 +0300

    changes for congratulations

--- src/components/congratsComponent/Congratulations.tsx
+++ src/components/congratsComponent/Congratulations.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import style from './Congratulations.module.css';
 
+type PropsType = {
+    score: number
+    handleFinish: () => void
+}
 
-const Congratulations = () => {
+const Congratulations: React.FC<PropsType> = ({score, handleFinish}) => {
 
     return (
         <div className={style.container}>
             <h1>Поздравляем!</h1>
-            <p>Вы прошли викторину и набрали <b>""</b> из <b>30</b> возможных баллов</p>
+            <p>Вы прошли викторину и набрали <b>"{score}"</b> из <b>30</b> возможных баллов</p>
             <hr/>
-            <button className={style.btn}>Попробовать ещё раз!</button>
+            <button className={style.btn} onClick={handleFinish}>Попробовать ещё раз!</button>
         </div>
     );
 };
