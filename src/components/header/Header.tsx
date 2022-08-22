d15a301965f5dad315016b03057f2c8fda22a87a
Ilya030290 <anoshko90@mail.ru>
2022-08-22 10:24:58 +0300

    changes for Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import TopPanel from "./topPanel/TopPanel";
 import BirdsList from "./birdsList/BirdsList";
+import {BirdsSectionType} from "../../types/types";
 import style from './Header.module.css';
+
 
 type HeaderPropsType = {
     score: number
     currentLevel: number
+    birdsData: BirdsSectionType[]
 }
 
-const Header: React.FC<HeaderPropsType> = ({score, currentLevel}) => {
+const Header: React.FC<HeaderPropsType> = ({score, currentLevel, birdsData}) => {
     return (
         <div className={style.headerContainer}>
             <TopPanel score={score} />
-            <BirdsList />
+            <BirdsList currentLevel={currentLevel} birdsData={birdsData}/>
         </div>
     );
 };
