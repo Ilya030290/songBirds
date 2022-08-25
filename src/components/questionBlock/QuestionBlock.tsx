2d61a7c121a1cbf7f3e286e3b9f123ee6a7680f3
Ilya030290 <anoshko90@mail.ru>
2022-08-25 10:34:59 +0300

    changes for QuestionBlock

--- src/components/questionBlock/QuestionBlock.tsx
+++ src/components/questionBlock/QuestionBlock.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import {useAppSelector} from "../../redux/store";
 import AudioPlayer from "./audioPlayer/AudioPlayer";
-import image from '../../assets/images/bird.06a46938.jpg';
 import style from './QuestionBlock.module.css';
 
+type QuestionPropsType = {
+    image: string
+    name: string
+}
 
-const QuestionBlock = () => {
+const QuestionBlock: React.FC<QuestionPropsType> = ({image, name}) => {
+
+    const {currentLevel, birdsData, questionBirdID} = useAppSelector(state => state.game);
+
     return (
         <div className={style.questionContainer}>
             <img src={image}  alt={'bird'}/>
             <div className={style.listContainer}>
                 <div className={style.list}>
-                    <h3>******</h3>
-                    <AudioPlayer/>
+                    <h3>{name}</h3>
+                    {
+                        questionBirdID &&
+                        <AudioPlayer audio={birdsData[currentLevel].birds[questionBirdID - 1].audio}
+                        />
+                    }
                 </div>
             </div>
         </div>
