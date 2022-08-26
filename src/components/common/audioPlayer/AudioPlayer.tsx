2cae4a35e2011250950736f89bd496b64b6033ee
Ilya030290 <anoshko90@mail.ru>
2022-08-26 09:47:33 +0300

    add new custom audioPlayer

--- src/components/common/audioPlayer/AudioPlayer.tsx
+++ src/components/common/audioPlayer/AudioPlayer.tsx
@@ -1,21 +1,172 @@
-import React from 'react';
-import ReactAudioPlayer from "react-audio-player";
+import React, {useEffect, useRef, useState} from 'react';
+import { BsPlayCircle, BsPauseCircle } from 'react-icons/bs';
 import style from "./AudioPlayer.module.css";
 
 
 type PropsType = {
     audio: string
+    isMatch: boolean
 }
 
-const AudioPlayer: React.FC<PropsType> = ({audio}) => {
+const AudioPlayer: React.FC<PropsType> = ({audio, isMatch}) => {
+
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [volume, setVolume] = useState(0.5);
+    const audioPlayer = useRef(null);
+    const progressBar = useRef(null);
+    const volumeBar = useRef(null);
+    const animationRef = useRef(null);
+
+    const handleLoadingMetaData = () => {
+        if (audioPlayer?.current) {
+            //@ts-ignore
+            const seconds = Math.floor(audioPlayer.current.duration);
+            setDuration(seconds);
+            //@ts-ignore
+            progressBar.current.max = seconds;
+        }
+    };
+
+    const changePlayerCurrentTime = () => {
+        //@ts-ignore
+        progressBar?.current.style.setProperty(
+            '--seek-before-width',
+            //@ts-ignore
+            `${(progressBar.current.value / duration) * 100}%`
+        );
+        //@ts-ignore
+        setCurrentTime(progressBar?.current.value);
+    };
+
+    const changePlayerVolume = () => {
+        //@ts-ignore
+        volumeBar?.current.style.setProperty(
+            '--volume',
+            //@ts-ignore
+            `${volumeBar.current.value * 100}%`
+        );
+        //@ts-ignore
+        setVolume(volumeBar?.current.value);
+    };
+
+    const whilePlaying = () => {
+        //@ts-ignore
+        progressBar.current.value = audioPlayer?.current?.currentTime;
+        changePlayerCurrentTime();
+        //@ts-ignore
+        animationRef.current = requestAnimationFrame(whilePlaying);
+    };
+
+    const changeRange = () => {
+        //@ts-ignore
+        audioPlayer.current.currentTime = progressBar?.current.value;
+        changePlayerCurrentTime();
+    };
+
+    const changeVolume = () => {
+        //@ts-ignore
+        audioPlayer.current.volume = volumeBar?.current.value;
+        changePlayerVolume();
+    };
+
+    const togglePlayPause = () => {
+        setIsPlaying(!isPlaying);
+
+        if (!isPlaying) {
+            //@ts-ignore
+            audioPlayer.current.play();
+            //@ts-ignore
+            animationRef.current = requestAnimationFrame(whilePlaying);
+        } else {
+            //@ts-ignore
+            audioPlayer.current.pause();
+            //@ts-ignore
+            cancelAnimationFrame(animationRef.current);
+        }
+    };
+
+    const stopPlaying = () => {
+        setIsPlaying(false);
+        //@ts-ignore
+        audioPlayer.current.pause();
+        //@ts-ignore
+        audioPlayer.current.currentTime = 0;
+    };
+
+    const formatTime = (time: number) => {
+        const isDoubleValuedNumber = time >= 10;
+
+        return isDoubleValuedNumber ? `${time}` : `0${time}`;
+    };
+
+    const calculateTime = (secs: number) => {
+        const minutes = Math.floor(secs / 60);
+        const returnedMinutes = formatTime(minutes);
+        const seconds = Math.floor(secs % 60);
+        const returnedSeconds = formatTime(seconds);
+
+        return `${returnedMinutes} : ${returnedSeconds}`;
+    };
+
+    useEffect(() => {
+        //@ts-ignore
+        audioPlayer.current.src = audio;
+        stopPlaying();
+    }, [audio]);
+
+    useEffect(() => {
+        changePlayerVolume();
+    }, []);
+
+    useEffect(() => {
+        if (isMatch) {
+            stopPlaying();
+        }
+    }, [isMatch]);
 
     return (
         <div className={style.audioPlayer}>
-            <ReactAudioPlayer
-                style={{width: '100%'}}
-                src={audio}
-                controls={true}
-            />
+            <audio
+                onLoadedMetadata={handleLoadingMetaData}
+                onEnded={stopPlaying}
+                ref={audioPlayer}
+                preload="metadata"
+            >
+                <source src={audio} type="audio/mp3" />
+                <track kind="captions" />
+            </audio>
+            <button className={style.toggle} onClick={togglePlayPause}>
+                {
+                    isPlaying
+                        ? <BsPauseCircle className={style.toggleButton} />
+                        : <BsPlayCircle className={style.toggleButton} />
+                }
+            </button>
+            <div className={style.progress}>
+                <input
+                    className={style.progressBar}
+                    onChange={changeRange}
+                    value={currentTime}
+                    ref={progressBar}
+                    type="range"
+                />
+                <div className={style.status}>
+                    <span>{calculateTime(currentTime)}</span>
+                    <span>{calculateTime(duration)}</span>
+                </div>
+                <input
+                    className={style.volumeBar}
+                    onChange={changeVolume}
+                    value={volume}
+                    ref={volumeBar}
+                    type="range"
+                    min={0}
+                    max={1}
+                    step={0.05}
+                />
+            </div>
         </div>
     );
 };
