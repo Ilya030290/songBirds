4c9386052cc35074d77ac1806e4bd17df1eeac45
Ilya030290 <anoshko90@mail.ru>
2022-08-29 09:38:46 +0300

    adaptive styles for Congratulations

--- src/components/congratsComponent/Congratulations.tsx
+++ src/components/congratsComponent/Congratulations.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import image from '../../assets/images/3cf25d28-1a75-4d76-916d-a70e4d963a43.jpg';
 import style from './Congratulations.module.css';
 
 type PropsType = {
@@ -12,7 +13,11 @@ const Congratulations: React.FC<PropsType> = ({score, handleFinish}) => {
         <div className={style.container}>
             <h1>Поздравляем!</h1>
             <p>Вы прошли викторину и набрали <b>{score}</b> из <b>30</b> возможных баллов</p>
-            <hr/>
+            {
+                score < 30
+                    ? <hr/>
+                    : <img src={image} alt={'winner'}/>
+            }
             <button className={style.btn} onClick={handleFinish}>Попробовать ещё раз!</button>
         </div>
     );
