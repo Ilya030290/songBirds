2a1fcd8b31bdb25291d91dfa4f5be8c48df4aea6
Ilya030290 <anoshko90@mail.ru>
2022-09-08 18:57:30 +0300

    fix imports

--- src/components/common/loader/Loader.tsx
+++ src/components/common/loader/Loader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from './Loader.module.scss';
 import {CircularProgress} from '@mui/material';
+import styles from './Loader.module.scss';
 
 
 const Loader = () => {
