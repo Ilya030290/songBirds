2a0bf40b0026b66ac09fd5f50fcc23d8afa24ec2
Ilya030290 <anoshko90@mail.ru>
2022-09-08 18:55:29 +0300

    add constants for appStatus, level, score

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,14 +1,25 @@
-export const statuses = {
+export const indicatorStatus = {
     default: 'default',
     fail: 'fail',
     success: 'success',
 };
 
 export const indicators = [
-    { id: 1, status: statuses.default },
-    { id: 2, status: statuses.default },
-    { id: 3, status: statuses.default },
-    { id: 4, status: statuses.default },
-    { id: 5, status: statuses.default },
-    { id: 6, status: statuses.default },
+    { id: 1, status: indicatorStatus.default },
+    { id: 2, status: indicatorStatus.default },
+    { id: 3, status: indicatorStatus.default },
+    { id: 4, status: indicatorStatus.default },
+    { id: 5, status: indicatorStatus.default },
+    { id: 6, status: indicatorStatus.default },
 ];
+
+export const LOADING = 'loading' as const;
+export const SUCCESS = 'success' as const;
+export const FAILED = 'failed' as const;
+
+export const MAX_CURRENT_SCORE = 5;
+export const MAX_CURRENT_LEVEL = 5;
+export const MAX_SCORE = 30;
+
+
+
