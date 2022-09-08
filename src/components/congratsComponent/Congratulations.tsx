3d123a913b2daa8a6fb0db6aa55a961f733e6777
Ilya030290 <anoshko90@mail.ru>
2022-09-08 19:14:02 +0300

    change props name, add constants, fix className

--- src/components/congratsComponent/Congratulations.tsx
+++ src/components/congratsComponent/Congratulations.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import {MAX_SCORE} from '../../constants/constants';
 import image from '../../assets/images/3cf25d28-1a75-4d76-916d-a70e4d963a43.jpg';
 import styles from './Congratulations.module.scss';
 
-type PropsType = {
+
+type CongratulationsPropsType = {
     score: number
     handleFinish: () => void
 }
 
-const Congratulations: React.FC<PropsType> = ({score, handleFinish}) => {
+const Congratulations: React.FC<CongratulationsPropsType> = ({score, handleFinish}) => {
 
     return (
         <div className={styles.container}>
             <h1>Поздравляем!</h1>
-            <p>Вы прошли викторину и набрали <b>{score}</b> из <b>30</b> возможных баллов</p>
+            <p>Вы прошли викторину и набрали <b>{score}</b> из <b>{MAX_SCORE}</b> возможных баллов</p>
             {
-                score < 30
+                score < MAX_SCORE
                     ? <hr/>
                     : <img src={image} alt={'winner'}/>
             }
-            <button className={styles.btn} onClick={handleFinish}>Попробовать ещё раз!</button>
+            <button className={styles.confirmationButton} onClick={handleFinish}>Попробовать ещё раз!</button>
         </div>
     );
 };
